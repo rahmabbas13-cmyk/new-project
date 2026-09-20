cd6a6bfc85a28859e313804e6d9d98dcf7ca591e
rahmabbas13-cmyk <rahmabbas13@gmail.com>
2026-09-20 16:44:44 +0500

    first code

---
+++ main.py
@@ -0,0 +1,63 @@
+# with open("data.txt", "r") as file:
+#     content = file.read()
+#     print(content)
+
+# with open("data.txt", "r") as file:
+#     content = file.readline()
+#     print(content)
+
+# with open("data.txt", "r") as file:
+#     content1 = file.readline()
+#     content2 = file.readline()
+#     content3 = file.readline()
+#     print(content1)
+#     print(content2)
+#     print(content3)
+
+# with open("data.txt", "r") as file:
+#     content1 = file.readline()
+#     content2 = file.readline()
+#     content3 = file.readline()
+#     # print(content1)
+#     print(content2)
+#     # print(content3)
+
+# with open("data.txt", "r") as file:
+#      content = file.readlines()
+#      print(content)
+
+# with open("data.txt", "r") as file:
+#     content = file.readlines()
+#     print(content[0])
+
+# with open("data.txt", "r") as file:
+#     content = file.readlines()
+#     print(content[2])
+
+# file = open("data.txt", "r")
+# content = file.read()
+# print(content)
+# file.close()
+
+# with open("data.txt", "r") as file:
+#     for line in file:
+#         print(line)
+        
+# with open("data.txt", "r") as file:
+#     for line in file:
+#         print(len(line))
+        
+# with open("students.txt", "w") as file:
+#     file.write("Shehram")
+    
+# with open("students.txt", "a") as file:
+#     file.write("Kahaan\n")
+
+# with open("employee.txt", "a") as file:
+#     file.write("Khurram\n")
+#     file.write("Ahsaan\n")
+    
+# with open("employee.txt", "r") as file:
+#     for line in file:
+#         print(line)    
+
